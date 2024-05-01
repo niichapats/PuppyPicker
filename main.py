416eec9d7d4bf3d66d21cf223e026f60d4c6e613
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-05-01 11:57:05 +0700

    main (for running the program)

---
+++ main.py
@@ -0,0 +1,7 @@
+"""File to launch the Puppy Picker application."""
+
+from controller import PuppyPickerController
+
+if __name__ == '__main__':
+    puppy_picker = PuppyPickerController()
+    puppy_picker.run()

396682b4f578e429f9c4d4b03d8d8a2c29025900
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-05-02 19:35:06 +0700

    edit graph manage

--- graph_manage.py
+++ graph_manage.py
@@ -1,10 +1,6 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-from pandas.plotting import scatter_matrix
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class GraphManage:

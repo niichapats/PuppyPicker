30958738148e84aea3e4f43691d4b04acaeff33b
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-05-03 16:51:13 +0700

    update next_button_handler

--- controller.py
+++ controller.py
@@ -21,6 +21,27 @@ class PuppyPickerController:
     def next_button_handler(self, page):
         if page == 1:
             self.view.find_breeds_page2(self.model.descriptive_lifespan())
+        elif page == 2:
+            self.view.run_find_breeds_page3()
+        elif page == 3:
+            prefer_list = self.view.get_user_prefer()
+            allowed_values = {'0', '1', '2', '3', 'small', 'medium', 'big', 'all'}
+            correct_value = True
+            for item in prefer_list:
+                if item not in allowed_values:
+                    correct_value = False
+            if '' in prefer_list:
+                self.view.inform_error('Please complete all required fields')
+            elif 'Select' in prefer_list:
+                self.view.inform_error('Please choose a size')
+            elif not correct_value:
+                self.view.inform_error('Please enter only 0-3')
+                self.view.clear_user_prefer()
+            else:
+                top_name, top_score = self.model.find_matching_breeds(prefer_list)
+                print(top_name)
+                print(top_score)
+                self.view.find_breeds_page4(top_name, top_score)
 
     def show_info_handler(self):
         print(self.view.user_preference('show_breed'))

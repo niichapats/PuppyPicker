d8127c1e47801e156e43bdc2ce10a9881e4efa66
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-05-03 16:50:51 +0700

    update method find_matching_breeds

--- model.py
+++ model.py
@@ -7,7 +7,25 @@ class PuppyPickerModel:
         self.df = GraphManage.load_data('breeds.csv')
 
     def find_matching_breeds(self, preference: list):
-        pass
+        columns = ['adaptability', 'all_around_friendliness', 'health_grooming',
+                   'trainability', 'exercise_needs', 'average_lifespan']
+        print(preference)
+        if preference[6] == 'all':
+            df_temp = self.df.copy()
+        else:
+            df_temp = self.df[self.df['size_category'] == preference[6]].copy()
+
+        for index in range(6):
+            df_temp['score_' + columns[index]] = df_temp[columns[index]] * int(preference[index])
+
+        df_temp['sum_score'] = df_temp['adaptability'] + df_temp['all_around_friendliness'] \
+                               + df_temp['health_grooming'] + df_temp['trainability'] \
+                               + df_temp['exercise_needs'] + df_temp['average_lifespan']
+
+        top_rows = df_temp.sort_values(by='sum_score', ascending=False).head(5)
+        top_name = top_rows['breed'].tolist()
+        top_score = top_rows['sum_score'].tolist()
+        return top_name, top_score
 
     def descriptive_lifespan(self):
         min_lifespan = self.df['average_lifespan'].min()

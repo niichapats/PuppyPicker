0ae88d557a254334819599de2a0c2c64b1a4f0be
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-05-11 10:53:59 +0700

    add docstring, fix pylint

--- graph_manage.py
+++ graph_manage.py
@@ -1,18 +1,26 @@
+"""
+Module for managing data visualization in the Puppy Picker
+"""
+
 import pandas as pd
 import seaborn as sns
 import numpy as np
-# import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
 
 class GraphManage:
+    """
+    Manages data loading, processing, and visualization
+    for the Puppy Picker.
+    """
     def __init__(self):
         self.df = self.load_data('breeds.csv')
 
     @staticmethod
     def load_data(filepath):
         """
-        Load and preprocess data from a CSV file.
+        Load and preprocess data from a CSV file, enhancing the dataset
+        with calculated fields like average lifespan and size category for detailed analysis.
         """
         df = pd.read_csv(filepath)
         df['average_lifespan'] = (df['min_life_expectancy'] + df['max_life_expectancy']) / 2
@@ -23,13 +31,14 @@ class GraphManage:
 
         bins = [0, 17.3125, 47.46875, 100.25]
         size_labels = ['small', 'medium', 'big']
-        df['size_category'] = pd.cut(df['average_size'], bins=bins, labels=size_labels, include_lowest=True)
-
+        df['size_category'] = pd.cut(df['average_size'], bins=bins, labels=size_labels,
+                                     include_lowest=True)
         return df
 
     def create_histogram(self, selected_var, size):
         """
-        Creates a histogram figure of the specified variable from the dataframe.
+        Creates a histogram figure of the specified variable
+        from the dataframe based on the specified size of the graph.
         """
         if size == 'small':
             fig = Figure(figsize=(4, 3))
@@ -54,24 +63,32 @@ class GraphManage:
             return fig
 
     def story_scatter(self):
-        if 'average_lifespan' in self.df.columns and 'average_size' in self.df.columns:
-            fig = Figure(figsize=(2.5, 2))
-            ax = fig.subplots()
+        """
+        Creates a scatter plot comparing average size and lifespan across
+        dog breeds in the dataset. This plot is used on the storytelling page
+        """
+        fig = Figure(figsize=(2.5, 2))
+        ax = fig.add_subplot(111)
 
-            sns.scatterplot(data=self.df, x='average_size', y='average_lifespan', ax=ax, color='#9370DB')
+        sns.scatterplot(data=self.df, x='average_size',
+                        y='average_lifespan', ax=ax, color='#9370DB')
 
-            ax.set_title('Size vs. Lifespan Scatter plot', fontsize=6)
-            ax.set_xlabel('Average Size (Height and Weight Combined)', fontsize=6)
-            ax.set_ylabel('Average Lifespan (Years)', fontsize=6)
-            ax.tick_params(axis='both', which='major', labelsize=6)
-            ax.grid(True)
-            fig.tight_layout(pad=0.5)
+        ax.set_title('Size vs. Lifespan Scatter plot', fontsize=6)
+        ax.set_xlabel('Average Size (Height and Weight Combined)', fontsize=6)
+        ax.set_ylabel('Average Lifespan (Years)', fontsize=6)
+        ax.tick_params(axis='both', which='major', labelsize=6)
+        ax.grid(True)
+        fig.tight_layout(pad=0.5)
 
-            return fig
+        return fig
 
     def story_heatmap(self):
+        """
+        Creates a heatmap showing correlations between average size and
+        lifespan within the breed dataset. This plot is used on the storytelling page
+        """
         fig = Figure(figsize=(2.5, 2))
-        ax = fig.subplots()
+        ax = fig.add_subplot(111)
 
         selected_columns = self.df[['average_lifespan', 'average_size']]
         correlation_matrix = selected_columns.corr()
@@ -87,8 +104,13 @@ class GraphManage:
         return fig
 
     def story_bar(self):
+        """
+        Creates a bar graph showing the average lifespan of dog breeds
+        categorized by size. This plot is used on the storytelling page
+        """
         self.df['size_category'] = pd.Categorical(self.df['size_category'],
-                                                  categories=['small', 'medium', 'big'], ordered=True)
+                                                  categories=['small', 'medium', 'big'],
+                                                  ordered=True)
 
         fig = Figure(figsize=(2, 2))
         ax = fig.add_subplot(111)
@@ -97,8 +119,8 @@ class GraphManage:
                                order=['small', 'medium', 'big'], ax=ax)
 
         colors = ['#27408B', '#008080', '#71C671']
-        for i, bar in enumerate(bar_plot.patches):
-            bar.set_color(colors[i % len(colors)])
+        for i, each_bar in enumerate(bar_plot.patches):
+            each_bar.set_color(colors[i % len(colors)])
         ax.set_title('Average Lifespan by Size Category', fontsize=6)
         ax.set_xlabel('Size Category', fontsize=6)
         ax.set_ylabel('Average Lifespan (Years)', fontsize=6)
@@ -108,6 +130,10 @@ class GraphManage:
         return fig
 
     def char_bar(self, breed):
+        """
+        Creates a bar graph displaying scores of various characteristics
+        for a specific dog breed. This plot is used on the Statistical Information page
+        """
 
         selected_breeds = [breed]
         characteristics = ['all_around_friendliness', 'trainability',
@@ -137,6 +163,10 @@ class GraphManage:
 
     @staticmethod
     def score_bar(x_axis, y_axis):
+        """
+        Creates a bar graph displaying scores of
+        various characteristics for a specific dog breed.
+        """
         fig = Figure(figsize=(3.5, 4))
         ax = fig.add_subplot(111)
 
@@ -156,6 +186,10 @@ class GraphManage:
         return fig
 
     def male_bar(self, breed):
+        """
+        Creates a bar graph illustrating physical measurements
+        for male dogs of a selected breed.
+        """
         x_axis = ['Min height', 'Max Height', 'Min Weight', 'Max Weight']
         min_height_m = self.df[self.df['breed'] == breed]['min_height_male'].iloc[0]
         max_height_m = self.df[self.df['breed'] == breed]['max_height_male'].iloc[0]
@@ -180,6 +214,10 @@ class GraphManage:
         return fig
 
     def female_bar(self, breed):
+        """
+        Creates a bar graph illustrating physical measurements
+        for female dogs of a selected breed.
+        """
         x_axis = ['Min height', 'Max Height', 'Min Weight', 'Max Weight']
         min_height_f = self.df[self.df['breed'] == breed]['min_height_female'].iloc[0]
         max_height_f = self.df[self.df['breed'] == breed]['max_height_female'].iloc[0]
@@ -204,8 +242,11 @@ class GraphManage:
         return fig
 
     def explore_bar(self, x_axis, y_axis):
+        """
+        Creates a bar graph based on user-selected attributes for exploratory data analysis.
+        """
         fig = Figure(figsize=(5.5, 3.5))
-        ax = fig.subplots()
+        ax = fig.add_subplot(111)
 
         sns.barplot(data=self.df, x=x_axis, y=y_axis, ax=ax, color='#E88989')
 
@@ -220,8 +261,11 @@ class GraphManage:
         return fig
 
     def explore_scatter(self, x_axis, y_axis):
+        """
+        Creates a scatter plot based on user-selected attributes for exploratory data analysis.
+        """
         fig = Figure(figsize=(5.5, 3.5))
-        ax = fig.subplots()
+        ax = fig.add_subplot(111)
 
         sns.scatterplot(data=self.df, x=x_axis, y=y_axis, ax=ax, color='#E694C7')
 
@@ -236,6 +280,10 @@ class GraphManage:
         return fig
 
     def explore_breed_group_histgram(self, selected_group, selected_attribute):
+        """
+        Creates a histogram for a selected breed group and attribute,
+        aiding in detailed data exploration.
+        """
         filtered_df = self.df[self.df['breed_group'] == selected_group]
         fig = Figure(figsize=(5.5, 3.5))
         ax = fig.add_subplot(111)
@@ -250,11 +298,15 @@ class GraphManage:
         return fig
 
     def compare_bar(self, breed1, breed2, compare):
+        """
+        Creates a comparison bar graph showing characteristics
+        of two selected dog breeds side by side.
+        """
         breed1_data = [self.df.loc[self.df['breed'] == breed1, comp].iloc[0] for comp in compare]
         breed2_data = [self.df.loc[self.df['breed'] == breed2, comp].iloc[0] for comp in compare]
 
         fig = Figure(figsize=(5.5, 3.7))
-        ax = fig.subplots()
+        ax = fig.add_subplot(111)
         x_axis = np.arange(len(compare))
         ax.bar(x_axis - 0.2, breed1_data, 0.4, label=breed1, color='#D1E1A4')
         ax.bar(x_axis + 0.2, breed2_data, 0.4, label=breed2, color='#6DCDBC')
@@ -270,6 +322,3 @@ class GraphManage:
         fig.tight_layout(pad=0.5)
 
         return fig
-
-
-
